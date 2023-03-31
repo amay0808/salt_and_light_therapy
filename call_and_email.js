0c4b9c2df535a09ba2685a7de602d61ad5df9c63
Adrian Mayfield <amay0808@protonmail.com>
2023-03-31 06:46:30 -0700

    JS

---
+++ call_and_email.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const emailLink = document.querySelector(".footer__email");
+  const emailAddress = "info@saltandlighttherapy.net";
+  const subject = "Hello There";
+  const body = "Dear Support,";
+
+  emailLink.setAttribute(
+    "href",
+    `mailto:${emailAddress}?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`
+  );
+
+  emailLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.open(emailLink.getAttribute("href"), "_blank");
+  });
+});
