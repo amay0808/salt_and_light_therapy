0ad3aff3527eff7e06bcdb1ea29b13f6d067e840
Adrian Mayfield <amay0808@protonmail.com>
2023-04-15 09:06:34 -0700

    updates

--- vendor/scripts/call_and_email.js
+++ vendor/scripts/call_and_email.js
@@ -15,4 +15,29 @@ document.addEventListener("DOMContentLoaded", function () {
     event.preventDefault();
     window.open(emailLink.getAttribute("href"), "_blank");
   });
+
+  // Add an event listener for form submission
+  const form = document.querySelector(".form");
+  // const textArea = document.querySelector("#largeTextBox");
+
+  form.addEventListener("submit", submitForm);
 });
+
+function submitForm(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const phoneNumber = document.getElementById("number-of-guests").value;
+  const email = document.getElementById("your-email").value;
+  const message = document.getElementById("largeTextBox").value;
+
+  const mailtoLink = `mailto:info@saltandlighttherapy.net?subject=Form Submission&body=Name: ${encodeURIComponent(
+    name
+  )}%0D%0AEmail: ${encodeURIComponent(
+    email
+  )}%0D%0APhone Number: ${encodeURIComponent(
+    phoneNumber
+  )}%0D%0AMessage: ${encodeURIComponent(message)}`;
+
+  window.open(mailtoLink, "_blank");
+}
